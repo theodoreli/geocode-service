69b7d2bfdd66642623fc9c37aa5a3e805dda179d
Theodore Li <theol88@gmail.com>
2018-03-07 01:10:58 -0800

    Use config file for sources

--- geocode/geocode_sources.py
+++ geocode/geocode_sources.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import configparser
+import json
 import os
+import os.path as op
 import string
 from functools import partial
+
+config = configparser.ConfigParser()
+config.read(op.join('geocode', 'sources.ini'))
+
+geocode_sources = {}
+for section in config.sections():
+    geocode_sources[section] = {}
+    geocode_sources[section]['url'] = config.get(section, 'url')
+    geocode_sources[section]['lat'] = json.loads(config.get(section, 'lat'))
+    geocode_sources[section]['long'] = json.loads(config.get(section, 'long'))
+
+print(geocode_sources)
 
 vars = {
     'api_key_googlemaps' : os.environ.get('API_KEY_GOOGLEMAPS'),
@@ -12,37 +27,11 @@ vars = {
 
 def get_url_gen():
 
-    urls = [
-            (('https://maps.googleapis.com/maps/api/geocode/json?'
-             'address={adr}&key={api_key_googlemaps}'),
-             ['results', 0, 'geometry', 'location', 'lat'],
-             ['results', 0, 'geometry', 'location', 'lng']),
-            ('https://geocoder.cit.api.here.com/6.2/geocode.json?app_id={api_key_here_id}&app_code={api_key_here_code}&searchtext={adr}',
-                ['Response', 'View', 0, 'Result', 0, 'Location', 'DisplayPosition', 'Latitude'],
-                ['Response', 'View', 0, 'Result', 0, 'Location', 'DisplayPosition', 'Longitude'],
-                )
-    ]
-
-    def partial_functor(x):
-        s = x[0].format
+    def partial_functor(source):
+        s = source['url'].format
         s = partial(s, **vars)
-        return s,x[1],x[2]
+        return s,source['lat'],source['long']
 
-    formated = [partial_functor(url) for url in urls]
+    formated = [partial_functor(geocode_sources[source]) for source in geocode_sources]
+
     return formated
-
-
-
-    #'https://map.googleapis.com/maps/api/geocode/json?address={adr}&key={api_key_googlemaps}'.format(api_key_googlemaps=api_key_googlemaps)
-
-
-
-    #return urls
-
-
-'''
-    urls.append('https://geocoder.cit.api.here.com/6.2/geocode.json?app_id={id}&app_code={code}&searchtext={adr}'.format(id=api_key_here_id,
-                                              code=api_key_here_code,
-                                              adr=values['adr']))
-                                              '''
-
